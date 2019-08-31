0fd55f319cc6b2b00f7fce3d507006188ecb0256
John Forster <john.forster@vodafone.com>
2019-08-31 21:13:57 +0100

    refactor server

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,11 +2,14 @@ import express, { Response, Request } from 'express';
 import path from 'path';
 
 const app = express();
-
 const isDev = process.env.NODE_ENV === 'development';
-const middlePath = isDev ? '../../build' : '';
-app.use(express.static(path.join(__dirname, middlePath, '/dist')));
 
+// Publicly expose the '/dist' folder
+const middlePath = isDev ? '../../build' : '';
+const publicPath = path.join(__dirname, middlePath, '/dist');
+app.use(express.static(publicPath));
+
+// Send index.html when visiting '/'
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, middlePath, '/dist/index.html'));
 });
