43a79d7a8f24d1e61ad9eebbb1d763fee9e531e7
John Forster <johnforster@bulb.co.uk>
2020-08-22 13:12:39 +0100

    Add manifest png icons

--- src/client/app/components/page/page.tsx
+++ src/client/app/components/page/page.tsx
@@ -1,19 +1,22 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import * as Styled from './styled';
 
-export interface PageProps {}
+// export interface PageProps {}
 
-interface PageState {}
+// interface PageState {}
 
-export default class Page extends PureComponent<PageProps, PageState> {
-  render(): JSX.Element {
-    return (
-      <Styled.PageContainer>
-        {/* <If condition={process.env.NODE_ENV === 'development'}>
-          <h1>Stop Being A Perfectionist!</h1>
-        </If> */}
-        {this.props.children}
-      </Styled.PageContainer>
-    );
-  }
-}
+// export default class Page extends PureComponent<PageProps, PageState> {
+//   render(): JSX.Element {
+//     return (
+//       <Styled.PageContainer>
+//         {/* <If condition={process.env.NODE_ENV === 'development'}>
+//           <h1>Stop Being A Perfectionist!</h1>
+//         </If> */}
+//         {this.props.children}
+//       </Styled.PageContainer>
+//     );
+//   }
+// }
+const Page = Styled.PageContainer;
+
+export default Page;
