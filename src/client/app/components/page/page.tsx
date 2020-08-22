ddf60f7d8e94eab1425e1532fccaa2a0991a3545
John Forster <johnforster@bulb.co.uk>
2020-08-22 13:13:56 +0100

    Revert "Add manifest png icons"
    This reverts commit 43a79d7a8f24d1e61ad9eebbb1d763fee9e531e7.

--- src/client/app/components/page/page.tsx
+++ src/client/app/components/page/page.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import * as Styled from './styled';
 
-// export interface PageProps {}
+export interface PageProps {}
 
-// interface PageState {}
+interface PageState {}
 
-// export default class Page extends PureComponent<PageProps, PageState> {
-//   render(): JSX.Element {
-//     return (
-//       <Styled.PageContainer>
-//         {/* <If condition={process.env.NODE_ENV === 'development'}>
-//           <h1>Stop Being A Perfectionist!</h1>
-//         </If> */}
-//         {this.props.children}
-//       </Styled.PageContainer>
-//     );
-//   }
-// }
-const Page = Styled.PageContainer;
-
-export default Page;
+export default class Page extends PureComponent<PageProps, PageState> {
+  render(): JSX.Element {
+    return (
+      <Styled.PageContainer>
+        {/* <If condition={process.env.NODE_ENV === 'development'}>
+          <h1>Stop Being A Perfectionist!</h1>
+        </If> */}
+        {this.props.children}
+      </Styled.PageContainer>
+    );
+  }
+}
